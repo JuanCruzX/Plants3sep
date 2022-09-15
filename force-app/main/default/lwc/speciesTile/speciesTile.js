7bba2f631925b0c1c4dfeca509c053cf14e98cf8
Juan Cruz <juancnuevo14@gmail.com>
2022-09-15 09:38:53 -0300

    Funcionalidad añadida al boton view details en la tab Species_Explorer de cada planta que hay en la base de datos

--- force-app/main/default/lwc/speciesTile/speciesTile.js
+++ force-app/main/default/lwc/speciesTile/speciesTile.js
@@ -1,5 +1,31 @@
 import { LightningElement, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class SpeciesTile extends LightningElement {
+export default class SpeciesTile extends NavigationMixin(LightningElement) {
   @api specie;
+
+  // specie.Locacion__c = "Indoors";
+  // specie.Locacion__c = "Outdoors";
+  // specie.Locacion__c = "Indoors, Outdoors";
+
+  get isOutdoors() {
+    return this.specie.Location__c.includes("Outdoors");
+  }
+
+  get isIndoors() {
+    return this.specie.Location__c.includes("Indoors");
+  }
+
+  navigateToRecordViewPage() {
+    // View a custom object record.
+    this[NavigationMixin.Navigate]({
+        type: 'standard__recordPage',
+        attributes: {
+            recordId: this.specie.Id,
+            objectApiName: 'Species__c', // objectApiName is optional
+            actionName: 'view'
+        }
+    });
+}
+
 }
