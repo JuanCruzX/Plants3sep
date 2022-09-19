4a533e13d91256e5a91a6bb0c78a162b83b218d8
Plants3sep <git@github.com:JuanCruzX/Plants3sep.git>
2022-09-19 01:08:08 -0300

    Input de busqueda en la tab Species_Explorer, se escriben 3 letras y se filtra la busqueda, modificación de clase apex SpeciesService y html y javascript de speciesList (lightning web component)

--- force-app/main/default/lwc/speciesList/speciesList.js
+++ force-app/main/default/lwc/speciesList/speciesList.js
@@ -1,7 +1,21 @@
-import { LightningElement, wire } from 'lwc';
-import getAllSpecies from "@salesforce/apex/SpeciesService.getAllSpecies"
+import { LightningElement, wire } from "lwc";
+import getFilteredSpecies from "@salesforce/apex/SpeciesService.getFilteredSpecies";
 
 export default class SpeciesList extends LightningElement {
-  @wire(getAllSpecies)
+  // PROPERTIES, GETTERS & SETTERS
+  searchText = "";
+
+  //LIFECYCLE HOOKS
+
+  //WIRE
+  @wire(getFilteredSpecies, { searchText: "$searchText" })
   species;
+
+  //METHODS
+  handleInputChange(event) {
+    const searchTextAux = event.target.value;
+    if (searchTextAux.length >= 3 || searchTextAux === "") {
+      this.searchText = searchTextAux;
+    }
+  }
 }
